feb179cc87b4be3754dfd661ddf3e187c42c2ab8
Shreeprasad Tripathy <111556063+SHREEPRASAD1996@users.noreply.github.com>
2022-10-28 11:46:20 -0700

    Create Main_Books_Management.java

---
+++ Lab(20.10.2022)/Main_Books_Management.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Main_Books_Management
+{
+ public static void main(String[] args) 
+ {
+	 Library obj= new Library();
+	 obj.addBook(582,"Java", "Pallavi Madam");
+	 obj.dispanceBook("Java");
+	 Library obj2= new Library();
+	 obj.addBook(259,"India", "Shreeprasad");
+	 obj.dispanceBook("India");
+  }
+}
