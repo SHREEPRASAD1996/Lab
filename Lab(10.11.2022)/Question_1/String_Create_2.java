4ae7c1f07f07074e3dc68c00db25c6c4c7ebdd93
Shreeprasad Tripathy <111556063+SHREEPRASAD1996@users.noreply.github.com>
2022-11-10 22:30:20 -0800

    Create String_Create_2.java

---
+++ Lab(10.11.2022)/Question_1/String_Create_2.java
@@ -0,0 +1,13 @@
+package com.java;
+//Write a program which creates a String “C:\IBM\DB2\PROGRAM\DB2COPY1.EXE”. It parses the string with the delimiter as ‘\’ and displays the String in the following format.
+
+public class String_Create_2 { // class
+	public static void main(String[] args) { // main method
+		StringBuilder str = new StringBuilder("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");// string builder class
+		System.out.println("Drive:" + str.substring(0, 3)); // printing 'Drive:C:\'
+		System.out.println("Folder:" + str.substring(3, 6).concat("||") + str.substring(7, 10).concat("||")
+				+ str.substring(11, 18)); // printing 'Folder:IBM||DB2||PROGRAM'
+		System.out.println("File:" + str.substring(20)); // printing 'File:B2COPY1.EXE'
+
+	}
+}
