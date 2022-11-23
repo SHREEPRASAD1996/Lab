92669b1fc1af1479fa804f67a99612ac4c997fa3
Shreeprasad Tripathy <111556063+SHREEPRASAD1996@users.noreply.github.com>
2022-11-22 21:26:53 -0800

    Update Val_duplicate.java

--- Lab(03.11.2022)/Val_duplicate.java
+++ Lab(03.11.2022)/Val_duplicate.java
@@ -1,6 +1,6 @@
 //Write a Java program to find the duplicate values of an array of integer values
 
-public class DuplicateValue { // class
+public class Val_duplicate { // class
 	public static void main(String[] args) { // main method
 		int[] arr = { 5, 2, 7, 7, 5 }; // initializing and defing the array
 		for (int i = 0; i < arr.length - 1; i++) { // for each condition
